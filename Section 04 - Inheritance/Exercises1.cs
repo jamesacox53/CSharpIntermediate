63a2185c340c0e37f0dca00d2d3ba387ef9a6cf9
jamesacox53 <jamesacox53@gmail.com>
2022-05-29 21:17:12 +0100

    Completed Section 4

---
+++ Section 04 - Inheritance/Exercises1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section04Namespace
+{
+    internal class Exercises1
+    {
+        static void Main(string[] args)
+        {
+            Exercise1();
+        }
+
+        public static void Exercise1()
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Section 04 - Inheritance/Stack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section04Namespace
+{
+    internal class Stack
+    {
+        List<Object> stack = new List<Object>();
+        
+        public void Push(object obj)
+        {
+            if (obj == null) throw new InvalidOperationException("Can't push null onto the stack");
+            
+            stack.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if (stack.Count == 0) throw (new InvalidOperationException("The Stack is empty so can't pop"));
+
+            object element = stack[stack.Count - 1];
+                
+            stack.RemoveAt(stack.Count - 1);
+
+            return element;
+        }
+
+        public void Clear()
+        {
+            stack = new List<Object>();
+        }
+
+    }
+}
