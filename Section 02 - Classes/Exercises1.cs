db6d668756e22aa9a0b84aac9a24c2fd91344241
jamesacox53 <jamesacox53@gmail.com>
2022-05-29 16:17:46 +0100

    Add project files.

---
+++ Section 02 - Classes/Exercises1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section02Namespace
+{
+    internal class Exercises1
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
